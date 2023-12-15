6a81cd72e162c1cc1651c5fe0aebdba4c659a6f6
teorver <paul.britko@gmail.com>
2023-12-16 00:59:28 +0100

    feat: create functionality to render cards on the page

--- src/app/index.js
+++ src/app/index.js
@@ -1,21 +1,24 @@
+import createCard from './showCard.js';
+
 const getPinterestData = async () => {
     const photos = await fetch('https://api.unsplash.com/photos/random/?client_id=5HxesvmDSHyOpCHGFTaK2E_qm_g_xkMj9DZ-KU5QOdQ&count=25').then((response) => response.json());
-
     return {
         photos,
     }
 }
 
 const runPinterestApplication = async () => {
-    const localStoragePhotos = localStorage.getItem('photos');
+    const localStoragePhotos = JSON.parse(localStorage.getItem('photos')) ?? [];
     if (!localStoragePhotos) {
         const { photos } = await getPinterestData();
         localStorage.setItem('photos', JSON.stringify(photos));
     }
 
-
     // Write your code
-
+    //createCard();
+    localStoragePhotos.forEach(value => {
+        createCard(value);
+    })
 }
 
-runPinterestApplication()
+runPinterestApplication();
