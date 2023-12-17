c75c5ea3e629bb9576276e54b350e13362c1fd6a
teorver <paul.britko@gmail.com>
2023-12-17 17:51:29 +0100

    feat: implement pop-up appearance on card click

--- src/app/showCard.js
+++ src/app/showCard.js
@@ -1,3 +1,42 @@
+// import createPopUp from "./showModal.js";
+//
+// const root = document.getElementById('root');
+//
+// export default function createCard(photo) {
+//     const cardWrapper = document.createElement('div');
+//     cardWrapper.classList.add('card');
+//
+//     const cardContent = document.createElement('img');
+//     cardContent.classList.add('card-content');
+//     cardContent.src = photo.urls.regular;
+//
+//     const descriptionWrapper = document.createElement('div');
+//     descriptionWrapper.classList.add('card-description');
+//
+//     let cardImg = document.createElement('img');
+//     cardImg.classList.add('card-image');
+//     cardImg.src = photo.user.profile_image.medium;
+//
+//     let cardDescription = document.createElement('span');
+//     cardDescription.classList.add('card-text');
+//
+//     if (photo.description) {
+//         cardDescription.textContent = `${photo.description}`;
+//     } else {
+//         cardDescription.textContent = `${photo.alt_description}`;
+//     }
+//
+//     descriptionWrapper.append(cardImg, cardDescription);
+//     cardWrapper.append(cardContent, descriptionWrapper);
+//     root.appendChild(cardWrapper);
+//
+//     cardContent.addEventListener('click', () => {
+//         createPopUp();
+//     });
+// }
+
+import createPopUp from "./showModal.js";
+
 const root = document.getElementById('root');
 
 export default function createCard(photo) {
@@ -7,9 +46,6 @@ export default function createCard(photo) {
     const cardContent = document.createElement('img');
     cardContent.classList.add('card-content');
     cardContent.src = photo.urls.regular;
-
-    const hoverIcon = document.createElement('div');
-    hoverIcon.classList.add('card-hover');
 
     const descriptionWrapper = document.createElement('div');
     descriptionWrapper.classList.add('card-description');
@@ -28,6 +64,10 @@ export default function createCard(photo) {
     }
 
     descriptionWrapper.append(cardImg, cardDescription);
-    cardWrapper.append(cardContent, hoverIcon, descriptionWrapper);
+    cardWrapper.append(cardContent, descriptionWrapper);
     root.appendChild(cardWrapper);
+
+    cardContent.addEventListener('click', () => {
+        createPopUp(cardContent);
+    });
 }
