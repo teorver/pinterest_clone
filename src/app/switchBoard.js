799424f6f94d4cd5884387cd22801447e2c8a89a
teorver <paul.britko@gmail.com>
2023-12-17 22:38:13 +0100

    refactor: update switchBoards file

--- src/app/switchBoard.js
+++ src/app/switchBoard.js
@@ -3,11 +3,13 @@ import createCard from "./showCard.js";
 const selectBoard = document.getElementById('select-board');
 const root = document.getElementById('root');
 
+// Render board according to the chosen value in select
 function showBoard() {
     selectBoard.addEventListener('change', () => {
         const selectedBoard = selectBoard.value;
         root.innerHTML = '';
 
+        // Specify cycle for the clicked value in select
         let boardData;
         if (selectedBoard === 'default__board') {
             boardData = JSON.parse(localStorage.getItem('photos')) ?? [];
